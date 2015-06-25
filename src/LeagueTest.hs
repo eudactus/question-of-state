f264e5f9084278efebed64ab005abc4af097e5e0
Germobar <edward.pond@springer.com>
2015-06-25 05:33:14 -0700

    remove commented out tests

--- src/LeagueTest.hs
+++ src/LeagueTest.hs
@@ -15,15 +15,6 @@ plymouthArgyle = "Plymouth Argyle"
 
 main :: IO ()
 main = hspec $ do
---    describe "resultPoints" $ do -- TODO replace with resultScores
---        it "scores 2 for a win" $ do
---            resultPoints celtic (MatchResult celtic 1 liverpool 0) `shouldBe` 2
---        it "scores 1 for a score draw" $ do
---            resultPoints celtic (MatchResult celtic 3 liverpool 3) `shouldBe` 1
---        it "scores 1 for a no score draw" $ do
---            resultPoints celtic (MatchResult celtic 0 liverpool 0) `shouldBe` 1
---        it "scores 0 for a lose" $ do
---            resultPoints celtic (MatchResult celtic 0 liverpool 3) `shouldBe` 0
     describe "calculateTable" $ do
         it "can score for one result" $ do
             let table = calculateTable [(MatchResult celtic 1 liverpool 0)]
