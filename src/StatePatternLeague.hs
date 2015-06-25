b189ae94125140f535c78f3c164a6e3571546ed5
Germobar <edward.pond@springer.com>
2015-06-25 04:55:08 -0700

    Tests pass

--- src/StatePatternLeague.hs
+++ src/StatePatternLeague.hs
@@ -9,22 +9,20 @@ type Points = Int
 type Score = (Team, Points)
 type LeagueTable = M.Map Team Points
 
-resultPoints :: Team -> MatchResult -> Points
-resultPoints team (MatchResult ta sa tb sb)
-    | team == winner = 2
-    | sa == sb = 1
-    | otherwise = 0
-    where winner = if sa > sb then ta else tb
-
-addResult :: LeagueTable -> MatchResult -> LeagueTable
-addResult table _ = table -- TODO
+resultScores :: MatchResult -> [Score]
+resultScores (MatchResult ta sa tb sb)
+    | sa > sb = [(ta, 2), (tb, 0)]
+    | sa < sb = [(ta, 0), (tb, 2)]
+    | otherwise = [(ta, 1), (tb, 1)]
 
 resultsScores :: [MatchResult] -> [Score]
-resultsScores _ = [] -- TODO
+resultsScores [] = []
+resultsScores (result:results) = (resultScores result) ++ (resultsScores results)
 
--- fold over list Score where acc is LeagueTable
 calculateTable :: [MatchResult] -> LeagueTable
 calculateTable results = foldl applyScore M.empty $ resultsScores results
 
 applyScore :: LeagueTable -> Score -> LeagueTable
-applyScore _ _ = M.empty -- TODO
+applyScore table (team, points) =
+    case M.lookup team table of Nothing -> M.insert team points table
+                                Just oldPoints -> M.adjust (\_ -> oldPoints + points) team table
