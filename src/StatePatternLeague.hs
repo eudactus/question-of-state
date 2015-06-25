6f852770eb2d5c82e6b27a0630c339b2c275d9a7
Germobar <edward.pond@springer.com>
2015-06-25 10:47:02 -0700

    refactor

--- src/StatePatternLeague.hs
+++ src/StatePatternLeague.hs
@@ -8,20 +8,20 @@ type Points = Int
 type Score = (Team, Points)
 type LeagueTable = M.Map Team Points
 
+calculateTable :: [MatchResult] -> LeagueTable
+calculateTable results = foldl updateTable M.empty $ resultsScores results
+
+resultsScores :: [MatchResult] -> [Score]
+resultsScores [] = []
+resultsScores (r:rs) = (resultScores r) ++ (resultsScores rs)
+
 resultScores :: MatchResult -> [Score]
 resultScores (MatchResult ta sa tb sb)
     | sa > sb   = [(ta, 2), (tb, 0)]
     | sa < sb   = [(ta, 0), (tb, 2)]
     | otherwise = [(ta, 1), (tb, 1)]
 
-resultsScores :: [MatchResult] -> [Score]
-resultsScores [] = []
-resultsScores (result:results) = (resultScores result) ++ (resultsScores results)
-
-calculateTable :: [MatchResult] -> LeagueTable
-calculateTable results = foldl applyScore M.empty $ resultsScores results
-
-applyScore :: LeagueTable -> Score -> LeagueTable
-applyScore table (team, points) =
+updateTable :: LeagueTable -> Score -> LeagueTable
+updateTable table (team, points) =
     case M.lookup team table of Nothing -> M.insert team points table
                                 Just oldPoints -> M.adjust (\_ -> oldPoints + points) team table
