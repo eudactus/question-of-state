4bea833631fe6ef9b81024d21193473acb3b87aa
paulb <paulb@ashridgetech.com>
2015-06-30 08:34:59 +0100

    Solution 4 - extended problem - fully working

--- src/Solution4.hs
+++ src/Solution4.hs
@@ -15,37 +15,44 @@ data TeamPosition = TeamPosition {teamPos :: Team, played :: Int, won :: Int, dr
 
 type CompetitionResults  = [TeamPosition]
 
---instance Show TeamPosition where
---  show p = (teamPos p) ++ " : " ++ (show $ won p) ++ (show $ drawn p) ++ (show $ lost p)
---        ++ (show $ goalsForPos p) ++ (show $ won p)
-
 resultPoints :: Team -> MatchResult -> TeamPoints
-resultPoints t mr
-  | (isWinner t mr) = TeamPoints { team = t, points = 3}
-  | (isDraw mr) = TeamPoints { team = t, points = 1}
-  | otherwise = TeamPoints { team = t, points = 0}
+resultPoints t (MatchResult ta sa tb sb) =
+    TeamPoints { team = t,
+                 goalsFor = if t == ta then sa else sb,
+                 goalsAgainst = if t == ta then sb else sa,
+                 points = points}
+  where points | isWinner = 3 | isDraw  = 1 | otherwise = 0
+        isWinner = (t == ta && sa > sb) || (t == tb && sb > sa)
+        isDraw = sa == sb
 
-isWinner :: Team -> MatchResult -> Bool
-isWinner t (MatchResult ta sa tb sb) = (t == ta && sa > sb) || (t == tb && sb > sa)
-
-isDraw :: MatchResult -> Bool
-isDraw (MatchResult _ sa _ sb) = sa == sb
 
 teams :: [MatchResult] -> [Team]
 teams results = L.nub(concat ([[(teamA m), (teamB m)] | m <- results]))
 
 teamPosition :: [TeamPoints] -> TeamPosition
-teamPosition tps = foldl addPoints TeamPosition{pointsPos = 0, played = 0} tps
-    where addPoints pos pts = TeamPosition { teamPos = (team pts),
+teamPosition tps = foldl addPoints startingPosition tps
+    where theTeam = (team $ head tps)
+          startingPosition = TeamPosition{teamPos = theTeam, pointsPos = 0, played = 0,won = 0, drawn = 0, lost = 0, goalsForPos = 0, goalsAgainstPos = 0, goalDifference = 0}
+          addPoints pos pts = TeamPosition {    teamPos = (teamPos pos),
                                                 pointsPos = (pointsPos pos) + (points pts),
                                                 played = (played pos) + 1,
-                                                won = 0, drawn = 0, lost = 0,
-                                                goalsForPos = 0, goalsAgainstPos = 0, goalDifference = 0}
+                                                won = (won pos) + if (goalsFor pts) > (goalsAgainst pts) then 1 else 0,
+                                                drawn = (drawn pos) + if (goalsFor pts) == (goalsAgainst pts) then 1 else 0,
+                                                lost = (lost pos) + if (goalsFor pts) < (goalsAgainst pts) then 1 else 0,
+                                                goalsForPos = (goalsForPos pos) + (goalsFor pts),
+                                                goalsAgainstPos = (goalsAgainstPos pos) + (goalsAgainst pts),
+                                                goalDifference = (goalDifference pos) + (goalsFor pts) - (goalsAgainst pts)}
 
 competitionResults :: [MatchResult] -> CompetitionResults
 competitionResults results = [positionForTeam t | t <- teams results]
-   where positionForTeam t = teamPosition [resultPoints t r | r <- results, t == (teamA r) || t == (teamB r)]
+   where positionForTeam t = teamPosition [resultPoints t r | r <- resultsWhereTeamPlayed t]
+         resultsWhereTeamPlayed t = [r | r <- results, t == (teamA r) || t == (teamB r)]
 
 leagueTable :: CompetitionResults -> CompetitionResults
 leagueTable tp = L.sortBy compareTeamPoints tp
   where compareTeamPoints t1 t2 = (pointsPos t2) `compare` (pointsPos t1)
+
+--instance Show TeamPosition where
+--  show p = (teamPos p) ++ " : " ++ (show $ won p) ++ (show $ drawn p) ++ (show $ lost p)
+--        ++ (show $ goalsForPos p) ++ (show $ won p)
+
