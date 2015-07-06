dddad6613858d0f456a51fdd07dadc20cc6213a8
Germobar <edward.pond@springer.com>
2015-07-06 14:20:40 -0700

    Towards typeclassing for scoring

--- src/Solution1.hs
+++ src/Solution1.hs
@@ -9,7 +9,25 @@ data MatchResult = MatchResult{teamA :: Team, scoreA :: Int, teamB :: Team, scor
 type Points = Int
 type Score = (Team, Points)
 type LeagueMap = M.Map Team Points
+-- eliminate
 type FootballScoring = (Int, Int) -> (Int, Int)
+newtype ThreePointsForAWin a = ThreePointsForAWin { getPoints :: (a, a) }
+
+class PairScoreable a where
+    score :: a -> (Int, Int)
+
+instance (Ord a) => PairScoreable (ThreePointsForAWin a) where
+    score (ThreePointsForAWin (sa, sb))
+        | sa > sb   = (3, 0)
+        | sa < sb   = (0, 3)
+        | otherwise = (1, 1)
+
+-- eliminate
+threePointsForAWin :: FootballScoring
+threePointsForAWin (sa, sb)
+    | sa > sb   = (3, 0)
+    | sa < sb   = (0, 3)
+    | otherwise = (1, 1)
 
 leagueTable :: [MatchResult] -> [Score]
 leagueTable results = reverse . sortBy (comparing snd) $ M.toList (calculateLeagueMap threePointsForAWin results)
@@ -19,16 +37,11 @@ calculateLeagueMap scoring results = foldl updateLeague M.empty $ resultsScores 
 
 resultsScores :: FootballScoring -> [MatchResult] -> [Score]
 resultsScores _ [] = []
-resultsScores s (r:rs) = (resultScores s r) ++ (resultsScores s rs)
+resultsScores s (r:rs) = (singleResultScores s r) ++ (resultsScores s rs)
 
-threePointsForAWin :: FootballScoring
-threePointsForAWin (sa, sb)
-    | sa > sb   = (3, 0)
-    | sa < sb   = (0, 3)
-    | otherwise = (1, 1)
-
-resultScores :: FootballScoring -> MatchResult -> [Score]
-resultScores scoring (MatchResult ta sa tb sb) =
+-- Introduce ThreePointsForAWin somehow
+singleResultScores :: FootballScoring -> MatchResult -> [Score]
+singleResultScores scoring (MatchResult ta sa tb sb) =
     let (ra, rb) = scoring (sa, sb)
         in [(ta, ra), (tb, rb)]
 
